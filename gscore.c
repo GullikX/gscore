d17ba7d1ecbd71707183e9e7dba019f1bc697916
Martin Gulliksson <martin@gullik.cc>
2020-08-17 10:00:08 +0000

    Added shaders

--- gscore.c
+++ gscore.c
@@ -3,10 +3,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+const char* const vertexShaderSource =
+    "#version 120\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = gl_Vertex;\n"
+    "}\0";
+
+
+const char* const fragmentShaderSource =
+    "#version 120\n"
+    "void main()\n"
+    "{\n"
+    "    gl_FragColor = vec4(0.2f, 0.5f, 1.0f, 1.0f);\n"
+    "}\0";
+
+
 void die(const char* const message) {
     printf("Error: %s\n", message);
     exit(EXIT_FAILURE);
 }
+
+
+GLuint createShader(const GLenum type, const char* const shaderSource) {
+    const char* const typeString = type == GL_VERTEX_SHADER ? "vertex" : "fragment";
+    GLuint shaderId = glCreateShader(type);
+    glShaderSource(shaderId, 1, &shaderSource, NULL);
+    glCompileShader(shaderId);
+
+    int compilationResult = -1;
+    glGetShaderiv(shaderId, GL_COMPILE_STATUS, &compilationResult);
+    if (!compilationResult) {
+        int messageLength = -1;
+        glGetShaderiv(shaderId, GL_INFO_LOG_LENGTH, &messageLength);
+        char* const message = calloc(messageLength, sizeof(char));
+        glGetShaderInfoLog(shaderId, messageLength, &messageLength, message);
+        printf("Failed to compile %s shader! Message:\n    %s\n", typeString, message);
+        free(message);
+        die("Failed to compile shader");
+    }
+    return shaderId;
+}
+
+
+GLuint createProgram() {
+    GLuint programId = glCreateProgram();
+
+    GLuint vertexShaderId = createShader(GL_VERTEX_SHADER, vertexShaderSource);
+    GLuint fragmentShaderId = createShader(GL_FRAGMENT_SHADER, fragmentShaderSource);
+
+    glAttachShader(programId, vertexShaderId);
+    glAttachShader(programId, fragmentShaderId);
+
+    glLinkProgram(programId);
+    glValidateProgram(programId);
+
+    glDeleteShader(vertexShaderId);
+    glDeleteShader(fragmentShaderId);
+
+    return programId;
+}
+
 
 int main() {
     if (!glfwInit()) {
@@ -24,6 +82,9 @@ int main() {
     if (glewInit() != GLEW_OK) {
         die("Failed to initialize GLEW");
     }
+
+    GLuint programId = createProgram();
+    glUseProgram(programId);
 
     float positions[6] = {
         -0.5f, -0.5f,
@@ -50,5 +111,6 @@ int main() {
         glfwPollEvents();
     }
 
+    glDeleteProgram(programId);
     glfwTerminate();
 }
