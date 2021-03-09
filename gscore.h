339bef3cda36114e9042f4a5ebf42b01f46b15de
Martin Gulliksson <martin@gullik.cc>
2021-03-09 19:44:36 +0000

    license: s/ Affero//g

--- gscore.h
+++ gscore.h
@@ -3,15 +3,15 @@
  * This file is part of gscore.
  *
  * gscore is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Affero General Public License
+ * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation, version 3.
  *
  * gscore is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Affero General Public License
+ * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>
  *
  */
